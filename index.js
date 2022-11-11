11065011ccfdfc726b083c59ae8fde6a6b4b814d
Braydon <BADelgado2003@outlook.com>
2022-11-11 13:12:43 -0500

    removed search tag function

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-function myFunction() {
+function searchBarToggle() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  function filterFunction() {
-    let input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByClassName("tag");
-    for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
-    }
   }
 let addPost = false;
 document.addEventListener("DOMContentLoaded", () => {
@@ -31,6 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
             postContainer.style.display = "none";
         }
     })
+    retrievePosts()
 })
 const baseURL = 'http://localhost:3000/posts'
 const postCollection = document.getElementById('post-collection')
@@ -86,9 +71,6 @@ function submitPost(event) {
   })
   .then(resp => resp.json())
   .then(resp => renderPosts(resp))
-  .catch((error) => {
-    console.error('There has been a problem with your fetch operation', error);
-  });
   name.value = ""
   message.value = ""
   tag.value = ""
